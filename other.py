aff79f469394928e5d5055796ef5fbb713773687
justanotherinternetguy <hendryxu1@gmail.com>
2022-11-09 22:11:11 -0500

    e
    e

--- other.py
+++ other.py
@@ -1,9 +1,18 @@
 import psycopg2
+import random
 
 conn = psycopg2.connect("dbname=hendrydb user=hendrytest password=hendrytest")
 cur = conn.cursor()
 
-cur.execute("insert into accounts values(0, 'bob123', 'bob123@gmail.com', 'abcde1234', '2022-11-9');")
+a = random.randint(0, 999)
+b = random.randint(0, 999)
+c = random.randint(0, 999)
+
+
+cur.execute("insert into accounts values({0}, 'bob123', 'bob123@gmail.com', 'abcde1234', '2022-11-9');").format(a))
+cur.execute("insert into accounts values(0, 'bob123', 'bob123@gmail.com', 'abcde1234', '2022-11-9');"))
+cur.execute("insert into accounts values(0, 'bob123', 'bob123@gmail.com', 'abcde1234', '2022-11-9');"))
+
 conn.commit()
 
 cur.execute("select * from accounts")
